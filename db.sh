dde73f6c2efb3cfc1fc5e57d7c55d2e649606bed
Jorge López <jorge@Kira.local>
2018-01-19 18:24:09 +0100

    issues with mysql pass

--- db.sh
+++ db.sh
@@ -12,13 +12,13 @@ FLUSH PRIVILEGES;" > /tmp/mysql_init;
 mysqld --skip-grant-tables --skip-networking --user=mysql --init-file=/tmp/mysql_init&
 sleep 5;
 mysql -u root -pd3m0P4ss++ -e "shutdown";
-rm -f /tmp/mysql_init;
 service mysqld start;
 mysql -u root -pd3m0P4ss++ -e "CREATE DATABASE wp; CREATE USER 'wordpress_user'@'%' IDENTIFIED BY '94-fP+*.pJ'; GRANT ALL PRIVILEGES ON wp.* TO 'wordpress_user'@'%'; FLUSH PRIVILEGES";
 iptables-save > /etc/sysconfig/iptables;
 sed -i /etc/sysconfig/iptables -e 's/-A IN_public_allow -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT/-A IN_public_allow -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT\n-A IN_public_allow -p tcp -m tcp --dport 80 -m conntrack --ctstate NEW -j ACCEPT/g';
 iptables-restore < /etc/sysconfig/iptables;
 echo "10.0.255.103 web web.demo.telecom-sudparis.eu" >> /etc/hosts;
+rm -f /tmp/mysql_init;
 myip=`curl -s http://whatismyip.host/ | grep -e ipaddress | grep -v N/A | sed -e 's/.*>\(.*\)<.*/\1/'`;
 
 #get db file
