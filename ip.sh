75a63f949e34ea075ca14f40c271ab671ffc846e
Jorge López <jorge@Kira.local>
2018-01-25 14:39:10 +0100

    updated ip file

--- ip.sh
+++ ip.sh
@@ -1,8 +1,8 @@
 myip=`curl -s http://whatismyip.host/ | grep -e ipaddress | grep -v N/A | sed -e 's/.*>\(.*\)<.*/\1/'`;
-mysql -u wordpress_user -p94-fP+*.pJ -h db.demo.carp.telecom-sudparis.eu -D wp -e "UPDATE wp_options SET option_value = replace(option_value, 'http://157.159.233.62/', 'http://$myip/') WHERE option_name = 'home' OR option_name = 'siteurl';";
+mysql -u wordpress_user -p94-fP+*.pJ -h db.demo.carp.telecom-sudparis.eu -D wp -e "UPDATE wp_options SET option_value = replace(option_value, 'http://157.159.233.59/', 'http://$myip/') WHERE option_name = 'home' OR option_name = 'siteurl';";
 
-mysql -u wordpress_user -p94-fP+*.pJ -h db.demo.carp.telecom-sudparis.eu -D wp -e "UPDATE wp_posts SET guid = replace(guid, 'http://157.159.233.62/','http://$myip/');";
+mysql -u wordpress_user -p94-fP+*.pJ -h db.demo.carp.telecom-sudparis.eu -D wp -e "UPDATE wp_posts SET guid = replace(guid, 'http://157.159.233.59/','http://$myip/');";
 
-mysql -u wordpress_user -p94-fP+*.pJ -h db.demo.carp.telecom-sudparis.eu -D wp -e "UPDATE wp_posts SET post_content = replace(post_content, 'http://157.159.233.62/','http://$myip/');";
+mysql -u wordpress_user -p94-fP+*.pJ -h db.demo.carp.telecom-sudparis.eu -D wp -e "UPDATE wp_posts SET post_content = replace(post_content, 'http://157.159.233.59/','http://$myip/');";
 
-mysql -u wordpress_user -p94-fP+*.pJ -h db.demo.carp.telecom-sudparis.eu -D wp -e "UPDATE wp_postmeta SET meta_value = replace(meta_value, 'http://157.159.233.62/','http://$myip/');"; 
+mysql -u wordpress_user -p94-fP+*.pJ -h db.demo.carp.telecom-sudparis.eu -D wp -e "UPDATE wp_postmeta SET meta_value = replace(meta_value, 'http://157.159.233.59/','http://$myip/');"; 
