b17f7e2fcbd91aad8476b852734a08064a01422d
Jorge López <jorge@Kira.local>
2018-02-01 11:12:30 +0100

    fixed firewall rules

--- db.sh
+++ db.sh
@@ -18,6 +18,13 @@ mysql -u root -pd3m0P4ss++ -e "CREATE DATABASE wp; CREATE USER 'wordpress_user'@
 iptables-save > /etc/sysconfig/iptables;
 sed -i /etc/sysconfig/iptables -e 's/-A IN_public_allow -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT/-A IN_public_allow -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT\n-A IN_public_allow -p tcp -m tcp --dport 3306 -m conntrack --ctstate NEW -j ACCEPT/g';
 iptables-restore < /etc/sysconfig/iptables;
+dnf -y install iptables-services;
+systemctl mask firewalld.service;
+systemctl enable iptables.service;
+systemctl enable ip6tables.service;
+systemctl stop firewalld.service;
+systemctl start iptables.service;
+systemctl start ip6tables.service;
 echo "10.0.255.151 web web.demo.telecom-sudparis.eu" >> /etc/hosts;
 rm -f /tmp/mysql_init;
 
@@ -25,4 +32,5 @@ rm -f /tmp/mysql_init;
 curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/jorgelopezcoronado/CARPDemo/master/db.dump > /tmp/db.dump
 mysql -u root -pd3m0P4ss++ -D wp < /tmp/db.dump
 rm -f /tmp/db.dump
+
 
